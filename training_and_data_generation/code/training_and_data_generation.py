5237b10b71535d843b4b7ad22bc3c6609aaa6179
ilhamraslan <31539704+ilhamraslan@users.noreply.github.com>
2020-04-21 10:49:41 +0100

    WIP

--- training_and_data_generation/code/training_and_data_generation.py
+++ training_and_data_generation/code/training_and_data_generation.py
@@ -16,12 +16,24 @@ def main():
         df_week_6_points = df.iloc[:,4*(i-1)+26:4*(i-1)+27]
         df_total = pd.concat([df_5_weeks,df_week_6_points],axis=1)
 
+        # appending to total training data
         end_list = df_total.values.tolist()
         df_training_data = df_training_data.append([pd.DataFrame(end_list)],ignore_index=True)
 
+        # writing 5 gameweeks of prediction data to file
+        df_prediction = pd.concat([df_id,df_5_weeks],axis=1)
+        df_prediction.to_csv("../../resources/prediction_data/gameweek_" + str(i+5) + "_prediction_data.csv",index=False)
+
+        # writing points at each of the gameweeks
+        df_points = pd.concat([df_id,df_week_6_points],axis=1)
+        df_points.to_csv("../../resources/points_data/gameweek_" + str(i+5) + "_points_data.csv",index=False)
+
     df_training_data.to_csv("../../resources/training_data.csv",index=False,header=False)
     print("Done")
+
     #algorithm to extract gameweek data for ML model from previous set
+
+
 
 if __name__=="__main__":
     main()
